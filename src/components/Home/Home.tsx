6adb5ff0c66d5e0f0ac5151701976f748899d083
Keiran <keiran.oleary@stacktracehq.com>
2020-08-18 07:23:37 +1000

    Update previews

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -30,56 +30,56 @@ const Home = () => {
       productId: "prod_HOdRUkZgB5CFmH",
       name: "Uh oh spaghetti oh",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HOdRUkZgB5CFmH",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Uh+oh+spaghetti+oh+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HOdRq4x692ERsA",
       name: "Grumplestiltskin",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HOdRq4x692ERsA",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Grumplestiltskin+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HmVUV2SprbAo9h",
       name: "Lullaby Angel",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HmVUV2SprbAo9h",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Lullaby+Angel+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HRh1JWoeQ6Etnj",
       name: "Bummer mummy",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HRh1JWoeQ6Etnj",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Bummer+mummy+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HRgyCTMF0a5tUo",
       name: "Butterfly flaps its wings",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HRgyCTMF0a5tUo",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Butterfly+flaps+its+wings+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HmVVaM78OF7B35",
       name: "The Wind",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HmVVaM78OF7B35",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/The+Wind+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HmVVnVWod9t63R",
       name: "Go to bed",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HmVVnVWod9t63R",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Go+to+bed+(preview).mp3",
       filetype: "audio",
     },
     {
       productId: "prod_HmVWh5V6yJyCBE",
       name: "Greetings song",
       url:
-        "https://api.happysingingkids.com/download/share/prod_HmVWh5V6yJyCBE",
+        "https://happy-singing-kids-preview.s3.amazonaws.com/Greetings+song+(preview).mp3",
       filetype: "audio",
     },
   ] as CartItem[];
