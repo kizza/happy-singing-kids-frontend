701c1020dcc1cbfa200f19caaa06f953fa6055b0
Keiran <keiran.oleary@stacktracehq.com>
2020-10-10 13:43:54 +1000

    Update temporary copy on preview page

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -42,7 +42,21 @@ const Dashboard = (props: Props) => {
   const instructions = () =>
     internal ? (
       <>
-        <p>Checkout the songs below and let me know what you think :)</p>
+        {/* <p>Checkout the songs below and let me know what you think :)</p> */}
+        <h2>
+          Kids songs you'll love to sing!
+          <br />
+          (they're a little bit different)
+        </h2>
+        <p>
+          Happy Singing Kids is the "covid project" of a musician and mother. A
+          labour of love, sharing the songs that come to life around the house
+          with her husband and two young daughters.
+        </p>
+        <p>
+          Here's hoping it brings your home, school or playground a bunch of
+          happy singing kids as well.
+        </p>
       </>
     ) : (
       <>
