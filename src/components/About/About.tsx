011809853198e5ae3bf2761a58563dd882d8b723
Keiran O'Leary <hello@keiranoleary.com>
2021-03-01 20:40:14 +1000

    Refactor about pay layout
    Use flex over absolute positioning

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,16 +6,20 @@ export default () => (
   <div className={styles.About}>
     <div className={styles.Inner}>
       {/* <h2>About</h2> */}
-      <p>
-        Happy Singing Kids is the "covid project" of a musician and mother. A
-        labour of love, sharing the songs that manifest around the house with
-        her husband and two young daughters.
+      <div className={styles.Text}>
+        <p>
+          Happy Singing Kids is the "covid project" of a musician and mother. A
+          labour of love, sharing the songs that manifest around the house with
+          her husband and two young daughters.
+        </p>
+        <p>
+          Here's hoping it brings your home, school or playground a bunch of
+          happy singing kids as well.
+        </p>
+      </div>
+      <p className={styles.Me}>
+        <img src={me} alt="Me" />
       </p>
-      <p>
-        Here's hoping it brings your home, school or playground a bunch of happy
-        singing kids as well.
-      </p>
-      <img src={me} className={styles.Me} alt="Me" />
     </div>
   </div>
 );
