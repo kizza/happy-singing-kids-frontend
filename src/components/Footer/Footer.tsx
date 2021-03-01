23c33d5539a98126ccfcca7ad3501ee98ebd28b9
Keiran O'Leary <hello@keiranoleary.com>
2021-03-02 08:50:49 +1000

    Add font-awesome 5

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,19 +6,22 @@ export default () => (
   <div className={styles.Footer}>
     <div className={styles.Social}>
       <a href="https://www.facebook.com/happysingingkids">
-        <i className="fa fa-lg fa-fw fa-facebook"></i>
+        <i className="fab fa-lg fa-fw fa-facebook"></i>
       </a>
       <a href="https://www.instagram.com/happysingingkids">
-        <i className="fa fa-lg fa-fw fa-instagram"></i>
+        <i className="fab fa-lg fa-fw fa-instagram"></i>
       </a>
       <a href={SPOTIFY_URL}>
-        <i className="fa fa-lg fa-fw fa-spotify"></i>
+        <i className="fab fa-lg fa-fw fa-spotify"></i>
       </a>
       <a href={AMAZON_URL}>
-        <i className="fa fa-lg fa-fw fa-amazon"></i>
+        <i className="fab fa-lg fa-fw fa-amazon"></i>
       </a>
       <a href="https://music.youtube.com/playlist?list=OLAK5uy_mukftXWpZWQY0lWq7-7uxL49OrAdbBpEc">
-        <i className="fa fa-lg fa-fw fa-youtube"></i>
+        <i className="fab fa-lg fa-fw fa-youtube"></i>
+      </a>
+      <a href="https://www.tiktok.com/@happysingingkids">
+        <i className="fab fa-lg fa-fw fa-tiktok"></i>
       </a>
     </div>
   </div>
