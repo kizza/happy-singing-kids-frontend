b4bc574f2b0020868c47bad19d177d88c69bb104
Keiran O'Leary <hello@keiranoleary.com>
2023-02-09 21:46:04 +1000

    Update footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { AMAZON_URL, SPOTIFY_URL } from "../../consts";
+import {AMAZON_URL, SPOTIFY_URL, TIKTOK_URL, YOUTUBE_URL} from "../../consts";
 import styles from "./Footer.module.scss";
 
-export default () => (
+const Footer = () => (
   <div className={styles.Footer}>
     <div className={styles.Social}>
       <a href="https://www.facebook.com/happysingingkids">
@@ -17,12 +17,14 @@ export default () => (
       <a href={AMAZON_URL}>
         <i className="fab fa-lg fa-fw fa-amazon"></i>
       </a>
-      <a href="https://music.youtube.com/playlist?list=OLAK5uy_mukftXWpZWQY0lWq7-7uxL49OrAdbBpEc">
+      <a href={YOUTUBE_URL}>
         <i className="fab fa-lg fa-fw fa-youtube"></i>
       </a>
-      <a href="https://www.tiktok.com/@happysingingkids">
+      <a href={TIKTOK_URL}>
         <i className="fab fa-lg fa-fw fa-tiktok"></i>
       </a>
     </div>
   </div>
 );
+
+export default Footer;
--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,12 @@
 export const SPOTIFY_URL =
-  "https://open.spotify.com/album/4vzOqW9ZCHRLva3VPgzMBP";
+  "https://open.spotify.com/artist/5pidckPd6eDXfUJmZiPeE9";
 export const AMAZON_URL =
   "https://music.amazon.com/albums/B08WH6SYP6?marketplaceId=ATVPDKIKX0DER";
 export const APPLE_URL =
   "https://music.apple.com/us/album/uh-oh-spaghetti-oh/1553353479";
 export const GOOGLE_URL =
   "https://music.youtube.com/playlist?list=OLAK5uy_mukftXWpZWQY0lWq7-7uxL49OrAdbBpEc";
+export const YOUTUBE_URL =
+  "https://www.youtube.com/@happysingingkids";
+export const TIKTOK_URL =
+  "https://www.tiktok.com/@happysingingkids";
