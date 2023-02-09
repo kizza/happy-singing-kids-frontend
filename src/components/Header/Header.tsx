f2027f7b8df82eb13eaa87ef1e4cb6cc3c9f079f
Keiran O'Leary <hello@keiranoleary.com>
2023-02-09 21:36:36 +1000

    Use svg burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,12 +10,14 @@ interface Props {
   openMenu: () => void;
 }
 
-export default ({ openMenu }: Props) => (
+const Header = ({ openMenu }: Props) => (
   <div className={styles.Header}>
     <div className={styles.Overlay}>
       <Logo className={styles.Logo} />
       <a className={styles.Hamburger} href="#menu" onMouseDown={openMenu}>
-        <i className="fa fa-fw fa-bars"></i>
+        <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
       </a>
     </div>
     {/* <div className={styles.Sky} /> */}
@@ -39,3 +41,5 @@ export default ({ openMenu }: Props) => (
     />
   </div>
 );
+
+export default Header;
