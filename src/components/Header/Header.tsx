9f1876add68374f93a95f813a30ac6ec7e16dd8a
Keiran O'Leary <hello@keiranoleary.com>
2023-08-30 12:02:18 +1000

    Update nav burger styles
    To be more visible in mobile.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ const Header = ({ openMenu }: Props) => (
       <Logo className={styles.Logo} />
       <a className={styles.Hamburger} href="#menu" onMouseDown={openMenu}>
         <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
       </a>
     </div>
