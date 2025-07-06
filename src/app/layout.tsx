abd8d211548d9935bfa3ce854fd5acccc287ab49
Keiran O'Leary <hello@keiranoleary.com>
2025-07-06 22:54:31 +1000

    Add brevo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,19 @@ export default function RootLayout({
           }}
         />
 
+        <script src="https://cdn.brevo.com/js/sdk-loader.js" async></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            // Version: 2.0
+            window.Brevo = window.Brevo || [];
+            Brevo.push([
+              "init", { client_key: "${process.env.NEXT_PUBLIC_BREVO_CLIENT_KEY}" }
+            ]);
+            `,
+          }}
+        />
+
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
