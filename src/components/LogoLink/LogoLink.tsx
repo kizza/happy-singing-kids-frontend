0d8879df127e6613782d319de88ef2205a7cc90d
Keiran O'Leary <hello@keiranoleary.com>
2026-06-08 22:45:43 +1000

    Tailwind logolink

--- src/components/LogoLink/LogoLink.tsx
+++ src/components/LogoLink/LogoLink.tsx
@@ -15,14 +15,26 @@ interface Props {
   label: string;
 }
 
+const underlineStyles = `
+  hover:after:content-['']
+  hover:after:absolute
+  hover:after:left-4
+  hover:after:right-4
+  hover:after:bottom-[0.8rem]
+  hover:after:border-b-4
+  hover:after:border-orange-500
+  hover:after:-rotate-1
+`
+
 const LogoLink = ({ styles: externalStyles, logo, url, label }: Props) => (
   <a href={url} className={classnames(
-    moduleStyles.LogoLink,
-    "flex",
+    // moduleStyles.LogoLink,
+    "flex relative overflow-hidden p-2 md:px-5 md:py-3",
+    underlineStyles,
     buttonStyles,
     ...(externalStyles || [])
   )} onClick={() => posthog.capture("streaming_platform_clicked", { platform: label, url })}>
-    <Image src={logo} alt={label} className="w-full" />
+    <Image src={logo} alt={label} className="w-auto max-w-full h-auto object-contain" />
   </a>
 );
 
